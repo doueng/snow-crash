c171b3417a39a53dca7e3473afa0e4ca1a026de5
Douglas ENGSTRAND <dengstra@e2r7p17.42.fr>
2019-04-19 15:57:53 +0200

    level08 cracked

---
+++ level08/Ressources/link.sh
@@ -0,0 +1,9 @@
+# level08 uses strstr to search the arg for 'token' if it returns true the
+# exe returns with an error msg.
+# To avoid that we create a symbolic link so when the strstr searches
+# the arg it doesn't find the 'token' substring but when 'open'
+# is called on the arg the link is followed and the token file in the home
+# folder is opened.
+ln -sf ~/token /tmp/hack
+./level08 /tmp/hack
+
